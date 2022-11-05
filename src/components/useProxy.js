37880eb7c81c5a97cab7d196571d58b06f30bd17
cazser <2264712072@qq.com>
2022-11-05 16:55:07 +0800

    +1

--- src/components/useProxy.js
+++ src/components/useProxy.js
@@ -1,13 +1,9 @@
 import { useState } from 'react'
 
 const useProxy = ([state, setState])=>{
-  const obj = {value: state};
-  return new Proxy(obj,
-    {
-      get:()=>{return obj.value},
-      set:(target, propKey, value, receiver)=>{
-       console.log(value); setState(value);}
-    } )
+//proxy只能拦截对象
+
+  
 }
 
 export default useProxy;
