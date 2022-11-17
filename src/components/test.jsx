2d28cbc57c3026c86d8e6ab8af5da9a099b22903
cazser <2264712072@qq.com>
2022-11-18 07:37:19 +0800

    只能浅响应

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import useProxy from './useProxy';
 const Test = ()=>{
- let count = useProxy( {age:19, name:"静静"} );
+ let count = useProxy( {age:19, name:"静静", education:{primary:"xxx小学"}} );
  const onClick = ()=>{ count.age = count.age+1}
   return (
      <div>
       <div>{count.age}</div>
       <div>{count.name}</div>
+   
       <button onClick={onClick} >+1</button>
      </div>
      )
