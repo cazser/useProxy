0eff3f2f5ed2744b21b1d9ff9fd0bc6686b65288
cazser <2264712072@qq.com>
2022-11-19 09:48:55 +0800

    +1

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -10,21 +10,13 @@ for(let num of array){
   console.log(num);
 }
 
-Object.getPrototypeOf(array).push;
-const count = useProxy({age:18, name:"Tom", education:{"primary":"xxx小学"}})
-const onclick1 = ()=>{
-  count.age++;
-  count.education.primary = count.age;
-} 
+
 return (
      <div>
       <ol>
         {array.map(item=><li>{item}</li> ) }
       </ol>
-      <button onClick={onClick} >+1</button>
-      <div>{count.age}</div>
-      <div>{count.education.primary}</div>
-      <button onClick={onclick1}>+1</button>
+      
      </div>
      )
 }
