f12f8c4cf4f0bc1b6a8ee96078a891585c6a5818
cazser <2264712072@qq.com>
2022-11-20 16:27:45 +0800

    createProxyForPlainObject.js

--- src/lib/useProxy/createProxyForPlainObject.js
+++ src/lib/useProxy/createProxyForPlainObject.js
@@ -4,13 +4,38 @@ const createProxyForPlainObject=(target, effect)=>{
     {
       //handler
       get:(target, key, receiver)=>{
+        console.log("get");
           return target[key];
       },
       set:(target, prop, val)=> { // 拦截写入操作
          effect(target);
           return true;
   
-      }
+      },
+      ownKeys:(target)=> {
+      return Object.keys(target);
+      },
+      getOwnPropertyDescriptor(target, prop) { // 被每个属性调用
+        return Object.getOwnPropertyDescriptor(target);
+      },
+        deleteProperty(target, prop) { // 拦截属性删除
+    
+        delete target[prop];
+        effect(target)
+        return true;
+    
+      },
+      has(target, prop) {
+        return prop in target
+      },
+      apply(target, thisArg, args) {
+      target.apply(thisArg, args)
+    }
+
+  
+
+
+
 
 
     })
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,10 @@ import App from './App'
 import './index.css'
 import createProxyForPlainObject from './lib/useProxy/createProxyForPlainObject';
 
-const object = createProxyForPlainObject({a:1, b:2}, ()=>{console.log("triger")})
-object.a=5;
-console.log(object);
+const object = createProxyForPlainObject([0,1,2], ()=>{console.log("triger")})
+console.log("*************")
+delete object[2]
+console.log("*************")
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
