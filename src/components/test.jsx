89eaa699938ae3bbc56c3c2b5489ba84743a12ee
cazser <2264712072@qq.com>
2022-11-24 18:09:57 +0800

    +1

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import useProxy from '../lib/useProxy/useProxy'
 
 const Test = ()=>{
-<<<<<<< HEAD
 
 const data = useProxy(new Set([1,2,3,4]))
 const onClick =()=>{
@@ -14,23 +13,11 @@ return (
       <ol>
         {Array.from(data).map((item, index)=><li key={index}>{item}</li>)}
       </ol>
-      <button onClick={onClick}>+1</button>   
-=======
-const data = useProxy({a:{b:{c:1}}})
-const onClick = ()=>{
-    data.a.b.c++;
+      <button onClick={onClick}>+1</button>
+      </div>   
+)
 }
-return (
-     <div>
-     Test
-     <div>
-        {data.a.b.c}
->>>>>>> 81af1a360ef8abad147286639ce140a4e499ba26
-     </div>
-     <button onClick={onClick}>+1</button>
-     </div>
-    )
-}
+
 
 
 export default Test;
