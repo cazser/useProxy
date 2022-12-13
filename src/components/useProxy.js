4da6b6f55d07eb5e334283aa4a05f0b3152037d7
cazser <2264712072@qq.com>
2022-12-13 13:57:06 +0800

    +1

--- src/components/useProxy.js
+++ src/components/useProxy.js
@@ -5,6 +5,17 @@ const useProxy = (obj)=>{
 
     //对象序列化
     //然后对比是否变化
-    //如果变化则调用setState }
+    //如果变化则调用setState 
+    let strobj = JSON.stringify(obj);//简单序列化有很多缺点
+    const [state, setState] = useState(obj);
+    return new Proxy(state,{
+        get(target, key){
+          return target[key]
+        },
+        set(target, prop, newVal){
+          target[prop] = newVal;
+        }
+    } )
+}
 
 export default useProxy;
