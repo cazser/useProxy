9e636c3ccf078097b982a9f650da008a28dfa47a
cazser <2264712072@qq.com>
2022-12-13 13:56:51 +0800

    +1

--- src/components/useProxy.js
+++ src/components/useProxy.js
@@ -1,9 +1,10 @@
 import { useState } from 'react'
 
 
-const useProxy = (arg)=>{
+const useProxy = (obj)=>{
 
-     
-}
+    //对象序列化
+    //然后对比是否变化
+    //如果变化则调用setState }
 
 export default useProxy;
