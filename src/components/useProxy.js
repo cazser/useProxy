f4b8f4119e958ecb68e9215bcc5102e9886bdc79
cazser <2264712072@qq.com>
2022-12-13 12:59:48 +0800

    +1

--- src/components/useProxy.js
+++ src/components/useProxy.js
@@ -30,46 +30,7 @@ if(typeof state!=='object'){
   });
 }else{
   //对象
-  let  serial = JSON.stringify(state);
-   return new Proxy(state, {
-     
-         get:function(target, prop, receiver) {
-             if(prop in target){
-              return target[prop];
-             }  
-
-             return undefined;
-        },
-        set(target, prop, val) { // 拦截写入操作
-         
-          if(prop in target){
-            target[prop] = val;
-          }
-          return true;
-        
-      },
-       ownKeys(target) {
-      return Object.keys(target);
-    },
-     getOwnPropertyDescriptor(target, prop) { // 被每个属性调用
-     return Object.getOwnPropertyDescriptor(target,prop);
-      },
-      deleteProperty(target, prop) { // 拦截属性删除
-    
-        delete target[prop];
-          return true;
-    
-      },
-
-      has(target, prop) {
-        return prop in target;
-    },
-    apply(target, thisArg, args) {
-       target.apply(thisArg, args);
-    }
-
-      
-  });
+   console.log("here") 
 }
 }
 
